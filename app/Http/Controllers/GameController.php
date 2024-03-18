aed75a0a0d6dd42715979928ed80aeb94139f182
jhonmeireles <jhonathan.oliveira@delage.com.br>
2024-03-18 20:27:32 -0300

    att

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -27,24 +27,105 @@ class GameController extends Controller
 
     public function rJSON()
     {
-        $client = new Client();
-        try {
-            // Make a request to the external endpoint
-            $response = $client->request('POST', 'https://api.gamesprime.fun/web-api/auth/session/v2/verifyOperatorPlayerSession', [
-                'verify' => false, // Apenas para desenvolvimento! Remove ou define como true em produção.
-                'headers' => [
-                    'Accept' => 'application/json',
+        return '{
+            "dt": {
+                "oj": {
+                    "jid": 0
+                },
+                "pid": "2z3oHNpTBq",
+                "pcd": "admin@demo.com",
+                "tk": "CC05AFC4-9059-49D3-B4DE-3DF2CDAADC47",
+                "st": 1,
+                "geu": "game-api/fortune-tiger/",
+                "lau": "/game-api/lobby/",
+                "bau": "web-api/game-proxy/",
+                "cc": "BRL",
+                "cs": "R$",
+                "nkn": "admin@demo.com",
+                "gm": [
+                    {
+                        "gid": 126,
+                        "msdt": 1638432092000,
+                        "medt": 1638432092000,
+                        "st": 1,
+                        "amsg": "",
+                        "rtp": {
+                            "df": {
+                                "min": 96.81,
+                                "max": 96.81
+                            }
+                        },
+                        "mxe": 2500,
+                        "mxehr": 8960913
+                    }
                 ],
-            ]);
+                "uiogc": {
+                    "bb": 0,
+                    "grtp": 1,
+                    "gec": 0,
+                    "cbu": 0,
+                    "cl": 0,
+                    "bf": 0,
+                    "mr": 0,
+                    "phtr": 0,
+                    "vc": 0,
+                    "bfbsi": 1,
+                    "bfbli": 1,
+                    "il": 0,
+                    "rp": 0,
+                    "gc": 1,
+                    "ign": 1,
+                    "tsn": 0,
+                    "we": 0,
+                    "gsc": 0,
+                    "bu": 0,
+                    "pwr": 0,
+                    "hd": 0,
+                    "et": 0,
+                    "np": 0,
+                    "igv": 0,
+                    "as": 0,
+                    "asc": 0,
+                    "std": 0,
+                    "hnp": 0,
+                    "ts": 0,
+                    "smpo": 0,
+                    "ivs": 1,
+                    "ir": 0,
+                    "hn": 1
+                },
+                "ec": [],
+                "occ": {
+                    "rurl": "",
+                    "tcm": "",
+                    "tsc": 0,
+                    "ttp": 0,
+                    "tlb": "",
+                    "trb": ""
+                },
+                "ioph": "dfa77bfd325a"
+            },
+            "err": null
+        }';
 
-            // Get the body of the response
-            $body = $response->getBody();
+        // $client = new Client();
+        // try {
+        //     // Make a request to the external endpoint
+        //     $response = $client->request('POST', 'https://api.gamesprime.fun/web-api/auth/session/v2/verifyOperatorPlayerSession', [
+        //         'verify' => false, // Apenas para desenvolvimento! Remove ou define como true em produção.
+        //         'headers' => [
+        //             'Accept' => 'application/json',
+        //         ],
+        //     ]);
 
-            // Return the JSON as a string
-            return $body;
-        } catch (\Exception $e) {
-            // Handle any exceptions and return an error message as JSON
-            return json_encode(['error' => $e->getMessage()]);
-        }
+        //     // Get the body of the response
+        //     $body = $response->getBody();
+
+        //     // Return the JSON as a string
+        //     return $body;
+        // } catch (\Exception $e) {
+        //     // Handle any exceptions and return an error message as JSON
+        //     return json_encode(['error' => $e->getMessage()]);
+        // }
     }
 }
